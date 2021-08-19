598461d52acb72c3eed29b6b38335e4197efb4a8
hkpark <phk0606@naver.com>
2021-08-19 18:39:39 +0900

    상품문의 페이지 생성

---
+++ src/components/shop/ProductComment.vue
@@ -0,0 +1,339 @@
+<template>
+  <div>
+    <v-container fluid>
+      <v-row justify="end">
+        <v-col cols="auto">
+          <!-- <v-btn dark color="purple">상품평 작성</v-btn> -->
+          <v-dialog v-model="dialog" persistent max-width="600px">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn color="purple" dark v-bind="attrs" v-on="on">
+                상품평 작성
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="text-h5"
+                  ><v-icon large color="indigo"> mdi-lead-pencil </v-icon>상품평
+                  작성하기</span
+                >
+              </v-card-title>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="6">
+                      <v-select
+                        :items="items"
+                        label="구매한 상품을 선택하세요."
+                        hide-details
+                        outlined
+                        dense
+                        :menu-props="{ offsetY: true }"
+                      />
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col>
+                      <validation-provider
+                        name="ProductComment"
+                        v-slot="{ errors }"
+                        rules="required|max:10"
+                      >
+                        <v-textarea
+                          rows="3"
+                          outlined
+                          v-model="productComment"
+                          label="상품평"
+                          :counter="10"
+                          :error-messages="errors"
+                        />
+                      </validation-provider>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-card>
+                      <v-card-title>평점</v-card-title>
+                      <v-card-text>
+                        <v-radio-group dense hide-details v-model="radios" row>
+                          <v-radio value="1">
+                            <template v-slot:label>
+                              매우 만족
+                              <v-rating
+                                background-color="indigo lighten-3"
+                                color="indigo"
+                                small
+                                readonly
+                                :value="5"
+                              />
+                            </template>
+                          </v-radio>
+                          <v-radio value="2">
+                            <template v-slot:label>
+                              만족
+                              <v-rating
+                                background-color="indigo lighten-3"
+                                color="indigo"
+                                small
+                                readonly
+                                :value="4"
+                              />
+                            </template>
+                          </v-radio>
+                          <v-radio value="3">
+                            <template v-slot:label>
+                              보통
+                              <v-rating
+                                background-color="indigo lighten-3"
+                                color="indigo"
+                                small
+                                readonly
+                                :value="3"
+                              />
+                            </template>
+                          </v-radio>
+                          <v-radio value="4">
+                            <template v-slot:label>
+                              불만
+                              <v-rating
+                                background-color="indigo lighten-3"
+                                color="indigo"
+                                small
+                                readonly
+                                :value="2"
+                              />
+                            </template>
+                          </v-radio>
+                          <v-radio value="5">
+                            <template v-slot:label>
+                              매우불만
+                              <v-rating
+                                background-color="indigo lighten-3"
+                                color="indigo"
+                                small
+                                readonly
+                                :value="1"
+                              />
+                            </template>
+                          </v-radio>
+                        </v-radio-group>
+                      </v-card-text>
+                    </v-card>
+                  </v-row>
+                  <v-row>
+                    <v-file-input
+                      @change="Preview_image"
+                      v-model="image"
+                      show-size
+                    />
+                  </v-row>
+                  <v-row>
+                    <v-img :src="url" max-width="300" />
+                  </v-row>
+                </v-container>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="blue darken-1" text @click="dialog = false">
+                  Close
+                </v-btn>
+                <v-btn color="blue darken-1" text @click="dialog = false">
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-expansion-panels>
+      <v-expansion-panel v-for="(item, i) in panelItems" :key="i">
+        <v-expansion-panel-header>
+          <v-row>
+            <v-col cols="2">
+              <v-img
+                src="//app-storage-edge-006.cafe24.com/bannermanage2/rooseoin0/2021/05/06/f0f36a4d1d86e805e2efd73894865765.jpg"
+                max-width="120"
+              />
+            </v-col>
+            <v-col cols="7">
+              <v-row>
+                <v-col cols="7" class="d-flex">
+                  <div class="pr-2">2021-08-17 12:30:02</div>
+
+                  <div>phk0606</div>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <div>
+                    사진과 다르게 색감이 좋으며, 입었을 때 스판 원단으로
+                    불편함이 없어요! 친절한 상담, 빠른 배송 감사합니다
+                  </div>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col cols="2" align-self="center">
+              <v-rating v-model="rating" color="indigo" small />
+            </v-col>
+          </v-row>
+        </v-expansion-panel-header>
+
+        <v-expansion-panel-content>
+          <v-row>
+            <v-col cols="2">
+              <v-img
+                src="//app-storage-edge-006.cafe24.com/bannermanage2/rooseoin0/2021/05/06/f0f36a4d1d86e805e2efd73894865765.jpg"
+                max-width="150"
+              />
+            </v-col>
+            <v-col cols="8">
+              <v-row>
+                <v-col>
+                  <div>
+                    사진과 다르게 색감이 좋으며, 입었을 때 스판 원단으로
+                    불편함이 없어요! 친절한 상담, 빠른 배송 감사합니다
+                  </div>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col cols="1" class="d-flex">
+              <v-btn outlined small color="purple" class="mr-2"> 수정 </v-btn>
+              <v-btn outlined small color="red"> 삭제 </v-btn>
+            </v-col>
+          </v-row>
+        </v-expansion-panel-content>
+        <v-card flat>
+          <v-row align="center">
+            <v-col cols="2">
+              <v-btn text @click.prevent="replistShowToggle(i)"
+                >작성된 댓글(1)</v-btn
+              >
+            </v-col>
+            <v-col>
+              <v-btn
+                outlined
+                small
+                color="indigo"
+                class="mr-2"
+                @click.prevent="replyRegistShow(i)"
+              >
+                작성
+              </v-btn>
+              <v-btn
+                outlined
+                small
+                color="orange"
+                @click.prevent="replyRegistHidden(i)"
+              >
+                취소
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-row
+            align="center"
+            ref="replyRegistRow"
+            v-show="false"
+            class="pb-1"
+          >
+            <v-col cols="1">
+              <v-spacer />
+            </v-col>
+            <v-col cols="7">
+              <v-textarea outlined hide-details rows="1" />
+            </v-col>
+            <v-col cols="4">
+              <v-btn dark color="deep-purple darken-3" class="mr-2">
+                등록
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-row ref="replyListRow" v-show="false" class="mt-0">
+            <v-col cols="1">
+              <v-spacer />
+            </v-col>
+            <v-col cols="10">
+              <v-card flat>
+                <v-card-subtitle> admin 2017-01-01 16:10 </v-card-subtitle>
+                <v-card-text>
+                  안녕하세요 고객님 저희 쇼핑몰을 이용해 주셔서 감사합니다.
+                  앞으로 더욱 노력하겠습니다.
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-expansion-panel>
+    </v-expansion-panels>
+    <v-container>
+      <v-row justify="center" class="pt-2" style="text-align: center">
+        <v-col cols="auto">
+          <pagination
+            :options="{
+              theme: 'bootstrap4',
+              edgeNavigation: true,
+              texts: {
+                first: '처음',
+                last: '마지막',
+                count: '전체 {count} 개중 {from} 부터 {to}  |{count} 개| 1 개',
+              },
+            }"
+            v-model="page"
+            :records="40"
+            :per-page="20"
+            @paginate="myCallback"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import Pagination from 'vue-pagination-2';
+export default {
+  data() {
+    return {
+      url: null,
+      image: null,
+      radios: 'Duckduckgo',
+      productComment: null,
+      page: 1,
+      panelItems: 5,
+      rating: 4,
+      dialog: false,
+      items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+    };
+  },
+  components: {
+    Pagination,
+  },
+  methods: {
+    replyRegistShow(index) {
+      console.log(index);
+      this.$refs.replyRegistRow[index].style.display = 'flex';
+    },
+    replyRegistHidden(index) {
+      console.log(index);
+      this.$refs.replyRegistRow[index].style.display = 'none';
+    },
+    replistShowToggle(index) {
+      if (this.$refs.replyListRow[index].style.display === 'none') {
+        this.$refs.replyListRow[index].style.display = 'flex';
+      } else {
+        this.$refs.replyListRow[index].style.display = 'none';
+      }
+    },
+    myCallback: function (page) {
+      console.log(`Page ${page} was selected. Do something about it`);
+    },
+    Preview_image(e) {
+      if (e !== null) {
+        this.url = URL.createObjectURL(this.image);
+      } else {
+        this.url = null;
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/shop/ProductQna.vue
@@ -0,0 +1,142 @@
+<template>
+  <div>
+    <v-expansion-panels>
+      <v-expansion-panel v-for="(item, i) in panelItems" :key="i">
+        <v-expansion-panel-header>
+          <v-row>
+            <v-col cols="auto">
+              {{ i }}
+            </v-col>
+            <v-col cols="6">
+              <div>색상이 2가지 뿐인데요. 다른 색은 없는 것인가요?...</div>
+            </v-col>
+
+            <v-col cols="2">
+              <div>phk0606</div>
+            </v-col>
+            <v-col cols="auto" class="d-flex">
+              <div class="pr-2">2021-08-17 12:30:02</div>
+            </v-col>
+          </v-row>
+        </v-expansion-panel-header>
+
+        <v-expansion-panel-content>
+          <v-row>
+            <v-col cols="7">
+              <div>Q. 색상이 2가지 뿐인데요. 다른 색은 없는 것인가요?...</div>
+
+              <div>A. 관리자에서 등록한 답변</div>
+            </v-col>
+            <v-col cols="1" class="d-flex">
+              <v-btn outlined small color="purple" class="mr-2"> 수정 </v-btn>
+              <v-btn outlined small color="red"> 삭제 </v-btn>
+            </v-col>
+          </v-row>
+        </v-expansion-panel-content>
+        <v-card flat>
+          <v-row align="center">
+            <v-col cols="1" />
+            <v-col cols="6">
+              답변 여부 : 답변 준비중 입니다. 신속하게 답변 드리겠습니다.
+            </v-col>
+          </v-row>
+          <v-row
+            align="center"
+            ref="replyRegistRow"
+            v-show="false"
+            class="pb-1"
+          >
+            <v-col cols="1">
+              <v-spacer />
+            </v-col>
+            <v-col cols="7">
+              <v-textarea outlined hide-details rows="1" />
+            </v-col>
+            <v-col cols="4">
+              <v-btn dark color="deep-purple darken-3" class="mr-2">
+                등록
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-row ref="replyListRow" v-show="false" class="mt-0">
+            <v-col cols="1">
+              <v-spacer />
+            </v-col>
+            <v-col cols="10">
+              <v-card flat>
+                <v-card-subtitle> admin 2017-01-01 16:10 </v-card-subtitle>
+                <v-card-text>
+                  안녕하세요 고객님 저희 쇼핑몰을 이용해 주셔서 감사합니다.
+                  앞으로 더욱 노력하겠습니다.
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-expansion-panel>
+    </v-expansion-panels>
+    <v-container>
+      <v-row justify="center" class="pt-2" style="text-align: center">
+        <v-col cols="auto">
+          <pagination
+            :options="{
+              theme: 'bootstrap4',
+              edgeNavigation: true,
+              texts: {
+                first: '처음',
+                last: '마지막',
+                count: '전체 {count} 개중 {from} 부터 {to}  |{count} 개| 1 개',
+              },
+            }"
+            v-model="page"
+            :records="40"
+            :per-page="20"
+            @paginate="myCallback"
+          />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col> 상품 문의하기 </v-col>
+      </v-row>
+      <v-row align="center">
+        <v-col cols="10">
+          <v-textarea
+            outlined
+            rows="3"
+            hide-details
+            clearable
+            placeholder="- 띄어쓰기를 포함하여 최대 1000자까지 작성할 수 있습니다.
+※ 욕설, 영업에 방해되는 글은 관리자에 의해 삭제됩니다.
+"
+          />
+        </v-col>
+        <v-col>
+          <v-btn large>등록</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import Pagination from 'vue-pagination-2';
+
+export default {
+  data() {
+    return {
+      panelItems: 5,
+      page: 1,
+    };
+  },
+  components: {
+    Pagination,
+  },
+  methods: {
+    myCallback: function (page) {
+      console.log(`Page ${page} was selected. Do something about it`);
+    },
+  },
+};
+</script>
+
+<style></style>
