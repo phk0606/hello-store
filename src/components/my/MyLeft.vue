67dfe47e98db282a7bd637ae5cfa2dbe1a760698
hkpark <phk0606@naver.com>
2021-08-22 00:32:35 +0900

    마이페이지 leftMenu 생성

--- src/components/my/MyLeft.vue
+++ src/components/my/MyLeft.vue
@@ -2,41 +2,114 @@
   <v-card class="mx-auto" max-width="400">
     <v-list-item class="indigo lighten-4">
       <v-list-item-content>
-        <v-list-item-title class="text-h6"> 마이페이지 </v-list-item-title>
+        <v-list-item-title class="text-subtitle-1">
+          마이페이지
+        </v-list-item-title>
       </v-list-item-content>
     </v-list-item>
+    <v-list dense>
+      <v-list-item-group>
+        <v-subheader>쇼핑 이용 정보</v-subheader>
+        <v-list-item>
+          <v-icon>mdi-menu-right</v-icon>
 
-    <v-divider />
-    <v-card-title>
-      <v-list-item-title class="text-subtitle-1">
-        쇼핑 이용 정보
-      </v-list-item-title>
-    </v-card-title>
-    <v-list dense nav>
-      <v-list-item>
-        <v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>주문 내역</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
           <v-icon>mdi-menu-right</v-icon>
-        </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title>주문 내역</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item>
-        <v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>장바구니</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
           <v-icon>mdi-menu-right</v-icon>
-        </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title>장바구니</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item>
-        <v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>포인트 내역</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+    <v-list dense>
+      <v-list-item-group>
+        <v-subheader>오픈숍 상품 관리</v-subheader>
+        <v-list-item>
           <v-icon>mdi-menu-right</v-icon>
-        </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title>포인트 내역</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+
+          <v-list-item-content>
+            <v-list-item-title>상품 등록하기</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-icon>mdi-menu-right</v-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>등록된 상품(00개)</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-icon>mdi-menu-right</v-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>주문 내역</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-icon>mdi-menu-right</v-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>판매 완료</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+    <v-list dense>
+      <v-list-item-group>
+        <v-subheader>게시판 이용 내역</v-subheader>
+        <v-list-item>
+          <v-icon>mdi-menu-right</v-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>1:1 문의</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-icon>mdi-menu-right</v-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>나의 상품평</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-icon>mdi-menu-right</v-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>나의 상품 문의</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+    <v-list dense>
+      <v-list-item-group>
+        <v-subheader>개인 정보 수정</v-subheader>
+        <v-list-item>
+          <v-icon>mdi-menu-right</v-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>개인 정보 수정</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-icon>mdi-menu-right</v-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>비밀 번호 변경</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
     </v-list>
   </v-card>
 </template>
