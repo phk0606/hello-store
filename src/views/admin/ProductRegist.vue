7b3f5a3df262a0ef8869adc3b33be5ed551f6b17
hkpark <phk0606@naver.com>
2021-08-22 23:23:36 +0900

    상품 등록 페이지 수정

--- src/views/admin/ProductRegist.vue
+++ src/views/admin/ProductRegist.vue
@@ -1,24 +1,44 @@
 <template>
   <v-container fluid>
-    <v-row>
+    <v-row dense align="center">
       <v-col cols="auto">
-        <v-breadcrumbs :items="items">
+        <v-icon>mdi-arrow-right-drop-circle-outline</v-icon>
+      </v-col>
+      <v-col cols="">
+        <v-breadcrumbs :items="items" class="pa-0">
           <template v-slot:divider>
             <v-icon>mdi-chevron-right</v-icon>
           </template>
         </v-breadcrumbs>
       </v-col>
     </v-row>
-    <v-row>
+    <v-row dense>
       <v-col>
-        <v-icon>mdi-drag-vertical</v-icon>
-        상품 등록
+        <span><v-icon>mdi-drag-vertical</v-icon> 상품 등록</span>
+      </v-col>
+    </v-row>
+    <v-row dense>
+      <v-col>
+        <span><v-icon>mdi-drag-vertical-variant</v-icon> 상품 정보 입력</span>
       </v-col>
     </v-row>
     <v-row>
+      <v-col cols="auto">
+        <v-select
+          :items="items1"
+          label="카테고리 선택"
+          outlined
+          dense
+          :menu-props="{ offsetY: true }"
+        />
+      </v-col>
       <v-col>
-        <v-icon>mdi-drag-vertical-variant</v-icon>
-        상품 정보 입력
+        <v-text-field
+          dense
+          label="상품명"
+          placeholder="상품명을 입력하세요."
+          outlined
+        />
       </v-col>
     </v-row>
   </v-container>
@@ -29,6 +49,7 @@ export default {
   name: 'ProductRegist',
   data() {
     return {
+      items1: ['Foo', 'Bar', 'Fizz', 'Buzz'],
       items: [
         {
           text: '상품 관리',
