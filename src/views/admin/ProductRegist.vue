23a101a1b4d11bb4597882a3509a77c9fb8a94d8
hkpark <phk0606@naver.com>
2021-08-22 21:59:36 +0900

    상품 등록 수정

--- src/views/admin/ProductRegist.vue
+++ src/views/admin/ProductRegist.vue
@@ -1,10 +1,48 @@
 <template>
-  <div>상품 등록</div>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="auto">
+        <v-breadcrumbs :items="items">
+          <template v-slot:divider>
+            <v-icon>mdi-chevron-right</v-icon>
+          </template>
+        </v-breadcrumbs>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-icon>mdi-drag-vertical</v-icon>
+        상품 등록
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-icon>mdi-drag-vertical-variant</v-icon>
+        상품 정보 입력
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'ProductRegist',
+  data() {
+    return {
+      items: [
+        {
+          text: '상품 관리',
+          disabled: false,
+          href: 'breadcrumbs_dashboard',
+        },
+        {
+          text: '상품 등록',
+          disabled: false,
+          href: 'breadcrumbs_link_1',
+        },
+      ],
+    };
+  },
 };
 </script>
 
