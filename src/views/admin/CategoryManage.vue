6d718439aedfc3bb65df1a3afc9452016cad0555
hkpark <phk0606@naver.com>
2021-08-24 17:35:15 +0900

    카테고리 목록 서버에서 받음

--- src/views/admin/CategoryManage.vue
+++ src/views/admin/CategoryManage.vue
@@ -37,15 +37,25 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col>
+      <v-col cols="4">
         <v-card class="mx-auto" max-width="400">
           <v-subheader class="indigo lighten-4">스타일숍</v-subheader>
-          <v-treeview dense activatable color="warning" :items="items2" />
+          <v-treeview
+            dense
+            activatable
+            color="warning"
+            :items="productCategories"
+          />
           <v-subheader class="indigo lighten-4">오픈숍</v-subheader>
-          <v-treeview dense activatable color="warning" :items="items2" />
+          <v-treeview
+            dense
+            activatable
+            color="warning"
+            :items="productCategories"
+          />
         </v-card>
       </v-col>
-      <v-col>
+      <v-col cols="8">
         <v-card>
           <v-row align="center" dense>
             <v-col cols="auto">1차 카테고리 명 </v-col>
@@ -90,8 +100,9 @@ import { getCategories } from '@/api/category';
 
 export default {
   async created() {
-    const { categories } = await getCategories();
-    console.log(categories);
+    const { data } = await getCategories();
+    console.log(data);
+    this.productCategories = data;
   },
   data() {
     return {
@@ -112,58 +123,7 @@ export default {
           href: 'breadcrumbs_link_1',
         },
       ],
-      items2: [
-        {
-          id: 1,
-          name: 'Outer',
-          children: [
-            { id: 2, name: '가디건' },
-            { id: 3, name: '자켓' },
-            { id: 4, name: '점퍼' },
-            { id: 5, name: '코트' },
-          ],
-        },
-        {
-          id: 6,
-          name: '상의',
-          children: [
-            { id: 7, name: '니트' },
-            { id: 8, name: '브라우스' },
-            { id: 9, name: '셔츠' },
-            { id: 10, name: '티' },
-          ],
-        },
-        {
-          id: 11,
-          name: '하의',
-          children: [
-            { id: 12, name: '슬랙스' },
-            { id: 13, name: '일자팬츠' },
-            { id: 14, name: '데님팬츠' },
-            { id: 15, name: '반바지' },
-          ],
-        },
-        {
-          id: 16,
-          name: '스커트',
-          children: [
-            { id: 17, name: '미니스커트' },
-            { id: 18, name: '롱스커트' },
-            { id: 19, name: '맥시스커트' },
-            { id: 20, name: 'A라인' },
-          ],
-        },
-        {
-          id: 21,
-          name: '원피스',
-          children: [
-            { id: 22, name: '캐주얼원피스' },
-            { id: 23, name: '정장원피스' },
-            { id: 24, name: '니트원피스' },
-            { id: 25, name: '청/데님원피스' },
-          ],
-        },
-      ],
+      productCategories: [],
     };
   },
 };
