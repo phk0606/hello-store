8c545be0b4c6baff853509894cec0737b1b364d1
hkpark <phk0606@naver.com>
2021-08-29 06:42:14 +0900

    상품 목록 수정

--- src/views/admin/ProductList.vue
+++ src/views/admin/ProductList.vue
@@ -14,121 +14,120 @@
     </v-row>
     <v-row dense>
       <v-col>
-        <span><v-icon>mdi-drag-vertical</v-icon> 스타일 숍 상품 리스트</span>
+        <v-chip label x-large color="white">
+          <v-icon left> mdi-chevron-right-box </v-icon>
+          스타일 숍 상품 리스트
+        </v-chip>
+      </v-col>
+    </v-row>
+
+    <v-row dense align="center" justify="center">
+      <v-col cols="2">단일 검색</v-col>
+      <v-col cols="4">
+        <v-select
+          :items="items1"
+          label=""
+          outlined
+          hide-details
+          dense
+          :menu-props="{ offsetY: true }"
+        />
+      </v-col>
+      <v-col cols="3">
+        <v-text-field dense hide-details outlined />
+      </v-col>
+      <v-col cols="3">
+        <v-btn small>상세 검색</v-btn>
+      </v-col>
+    </v-row>
+    <v-row dense align="center" justify="center">
+      <v-col cols="2">카테고리 선택</v-col>
+      <v-col cols="3">
+        <v-select
+          :items="items1"
+          label=""
+          outlined
+          hide-details
+          dense
+          :menu-props="{ offsetY: true }"
+        />
+      </v-col>
+
+      <v-col cols="3">
+        <v-select
+          :items="items1"
+          label=""
+          outlined
+          hide-details
+          dense
+          :menu-props="{ offsetY: true }"
+        />
+      </v-col>
+      <v-col cols="auto">판매 가격</v-col>
+      <v-col>
+        <v-text-field dense hide-details outlined />
+      </v-col>
+      <v-col>
+        <v-text-field dense hide-details outlined />
+      </v-col>
+    </v-row>
+    <v-row dense align="center" justify="center">
+      <v-col cols="auto">상품 등록일</v-col>
+      <v-col>
+        <v-menu
+          v-model="menu"
+          :close-on-content-click="false"
+          :nudge-right="40"
+          transition="scale-transition"
+          offset-y
+          min-width="auto"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="date"
+              label="Picker without buttons"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+            />
+          </template>
+          <v-date-picker v-model="date" @input="menu = false" />
+        </v-menu>
+      </v-col>
+      <v-col>
+        <v-menu
+          v-model="menu2"
+          :close-on-content-click="false"
+          :nudge-right="40"
+          transition="scale-transition"
+          offset-y
+          min-width="auto"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="date2"
+              label="Picker without buttons"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+            />
+          </template>
+          <v-date-picker v-model="date" @input="menu2 = false" />
+        </v-menu>
+      </v-col>
+      <v-col cols="auto">진열 여부</v-col>
+      <v-col class="d-flex">
+        <v-checkbox dense hide-details label="진열" class="mr-2" />
+        <v-checkbox dense hide-details label="품절" class="mr-2" />
+        <v-checkbox dense hide-details label="숨김" />
       </v-col>
     </v-row>
     <v-row>
-      <v-col>
-        <v-card>
-          <v-row dense align="center" justify="center">
-            <v-col cols="auto">단일 검색</v-col>
-            <v-col cols="3">
-              <v-select
-                :items="items1"
-                label=""
-                outlined
-                hide-details
-                dense
-                :menu-props="{ offsetY: true }"
-              />
-            </v-col>
-            <v-col cols="3">
-              <v-text-field dense hide-details outlined />
-            </v-col>
-            <v-col>
-              <v-btn small>상세 검색</v-btn>
-            </v-col>
-          </v-row>
-          <v-row dense align="center" justify="center">
-            <v-col cols="auto">카테고리 선택</v-col>
-            <v-col cols="3">
-              <v-select
-                :items="items1"
-                label=""
-                outlined
-                hide-details
-                dense
-                :menu-props="{ offsetY: true }"
-              />
-            </v-col>
-
-            <v-col cols="3">
-              <v-select
-                :items="items1"
-                label=""
-                outlined
-                hide-details
-                dense
-                :menu-props="{ offsetY: true }"
-              />
-            </v-col>
-            <v-col cols="auto">판매 가격</v-col>
-            <v-col>
-              <v-text-field dense hide-details outlined />
-            </v-col>
-            <v-col>
-              <v-text-field dense hide-details outlined />
-            </v-col>
-          </v-row>
-          <v-row dense align="center" justify="center">
-            <v-col cols="auto">상품 등록일</v-col>
-            <v-col>
-              <v-menu
-                v-model="menu"
-                :close-on-content-click="false"
-                :nudge-right="40"
-                transition="scale-transition"
-                offset-y
-                min-width="auto"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
-                    v-model="date"
-                    label="Picker without buttons"
-                    prepend-icon="mdi-calendar"
-                    readonly
-                    v-bind="attrs"
-                    v-on="on"
-                  />
-                </template>
-                <v-date-picker v-model="date" @input="menu = false" />
-              </v-menu>
-            </v-col>
-            <v-col>
-              <v-menu
-                v-model="menu2"
-                :close-on-content-click="false"
-                :nudge-right="40"
-                transition="scale-transition"
-                offset-y
-                min-width="auto"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
-                    v-model="date2"
-                    label="Picker without buttons"
-                    prepend-icon="mdi-calendar"
-                    readonly
-                    v-bind="attrs"
-                    v-on="on"
-                  />
-                </template>
-                <v-date-picker v-model="date" @input="menu2 = false" />
-              </v-menu>
-            </v-col>
-            <v-col cols="auto">진열 여부</v-col>
-            <v-col class="d-flex">
-              <v-checkbox dense hide-details label="진열" class="mr-2" />
-              <v-checkbox dense hide-details label="품절" class="mr-2" />
-              <v-checkbox dense hide-details label="숨김" />
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col><v-btn>검색</v-btn></v-col>
-          </v-row>
-        </v-card>
-      </v-col>
+      <v-col><v-btn>검색</v-btn></v-col>
     </v-row>
+
     <v-row>
       <v-col>총 등록 상품: 0000개/검색된 상품: 0000개</v-col>
     </v-row>
