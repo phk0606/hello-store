372b4b460a7ed2443073708e11aace7682090a71
hkpark <phk0606@naver.com>
2021-09-15 12:45:56 +0900

    주문 상세 내역 수정

--- src/views/myPage/OrderDetailInfo.vue
+++ src/views/myPage/OrderDetailInfo.vue
@@ -24,39 +24,48 @@
     </v-row>
     <v-row>
       <v-col cols="12">
-        <v-simple-table fixed-header dense height="200px">
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th class="text-center">번호</th>
-                <th class="text-center">상품정보</th>
-                <th class="text-center">수량</th>
-                <th class="text-center">판매가격</th>
-                <th class="text-center">배송비</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(order, index) in orders" :key="index">
-                <td class="text-center">{{ index + 1 }}</td>
-                <td>
-                  <v-row>
-                    <v-col cols="3">
-                      <v-img :src="order.imgSrc" max-width="100" />
-                    </v-col>
-                    <v-col>
-                      <div>상품명: {{ order.productName }}</div>
-                      <div>색상: {{ order.option1 }}</div>
-                      <div>디자인: {{ order.option2 }}</div>
-                    </v-col>
-                  </v-row>
-                </td>
-                <td class="text-center">{{ order.quantity }}</td>
-                <td class="text-center">{{ order.price }}</td>
-                <td class="text-center">{{ order.deliveryFee }}</td>
-              </tr>
-            </tbody>
+        <v-data-table
+          hide-default-footer
+          v-model="selected"
+          :headers="headers"
+          :items="orderProducts"
+          item-key="orderProductId"
+          show-select
+          class="elevation-1"
+          disable-sort
+        >
+          <template v-slot:[`item.image`]="{ item }">
+            <v-container>
+              <v-row>
+                <v-col>
+                  <v-img
+                    class="mx-auto"
+                    :src="'data:image/png;base64,' + item.image"
+                    style="width: 100px; height: 100px"
+                  />
+                </v-col>
+              </v-row>
+            </v-container>
           </template>
-        </v-simple-table>
+          <template v-slot:[`item.productName`]="{ item }">
+            <v-row>상품명: {{ item.productName }}</v-row>
+            <v-row v-if="item.productOptions && item.productOptions.length >= 1"
+              >{{ item.productOptions[0].optionName }}:
+              {{ item.productOptions[0].optionValue }}</v-row
+            >
+            <v-row v-if="item.productOptions && item.productOptions.length >= 2"
+              >{{ item.productOptions[1].optionName }}:
+              {{ item.productOptions[1].optionValue }}</v-row
+            >
+          </template>
+        </v-data-table>
+        <v-divider />
+        <template>
+          <v-row>
+            <v-col cols="10"> 총 상품 금액 </v-col>
+            <v-col>{{ sumField('totalPrice') }}</v-col>
+          </v-row>
+        </template>
       </v-col>
     </v-row>
     <v-row>
@@ -228,23 +237,88 @@
 </template>
 
 <script>
+import { getOrder } from '@/api/order';
+
 export default {
   name: 'OrderDetailInfo',
+  created() {
+    const orderId = this.$route.params.orderId;
+    this.getOrder(orderId);
+    this.orderId = orderId;
+  },
+  methods: {
+    async getOrder(orderId) {
+      try {
+        const { data } = await getOrder({
+          orderId: orderId,
+        });
+        console.log(data);
+        this.name = data.name;
+        this.phoneNumber = data.phoneNumber;
+        this.orderProducts = data.orderProducts;
+        this.requirement = data.requirement;
+        this.recipientName = data.recipientName;
+        this.recipientPhoneNumber = data.recipientPhoneNumber;
+        this.fullAddress =
+          data.address.address + ' ' + data.address.detailAddress;
+
+        let paymentMethodType = data.paymentMethodType;
+        if (paymentMethodType === 'WITHOUT_BANKBOOK') {
+          paymentMethodType = '무통장 입금';
+        } else if (paymentMethodType === 'CREDIT_CARD') {
+          paymentMethodType = '카드 결제';
+        } else if (paymentMethodType === 'ACCOUNT_TRANSFER') {
+          paymentMethodType = '계좌 이체';
+        }
+        this.paymentMethodType = paymentMethodType;
+        this.paymentPrice = data.paymentPrice;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    sumField(key) {
+      // sum data in give key (property)
+      return this.orderProducts.reduce((a, b) => a + (b[key] || 0), 0);
+    },
+  },
   data() {
     return {
-      orders: [
+      orderId: '',
+      headers: [
+        { text: '번호', align: 'center', value: 'orderProductId' },
+        { text: '이미지', align: 'center', sortable: false, value: 'image' },
+        { text: '상품 정보', align: 'center', value: 'productName' },
+        { text: '판매 가격', align: 'center', value: 'salePrice' },
+        { text: '수량', align: 'center', value: 'quantity' },
+        { text: '포인트', align: 'center', value: 'point' },
+        { text: '배송비', align: 'center', value: 'shippingFee' },
+        { text: '합계', align: 'center', value: 'totalPrice' },
+      ],
+      orderProducts: [
         {
-          productId: 12345,
-          productName: '백프린팅 반팔티셔츠1',
-          imgSrc:
-            '//rooseoin.co.kr/web/product/tiny/202108/030e628575b77813bf8c1efc126dfd7e.webp',
-          quantity: 1,
-          price: 12000,
-          option1: '화이트',
-          option2: 'C형',
-          deliveryFee: 2500,
+          image: null,
+          productId: null,
+          productName: '',
+          salePrice: null,
+          orderQuantity: null,
+          orderShippingFee: null,
+          totalPrice: null,
+          point: null,
+          orderProductOptions: [
+            {
+              optionGroupNumber: 1,
+              optionName: this.firstOptionName,
+              optionValue: this.firstOptionValue,
+            },
+            {
+              optionGroupNumber: 2,
+              optionName: this.secondOptionName,
+              optionValue: this.secondOptionValue,
+            },
+          ],
         },
       ],
+      selected: [],
     };
   },
 };
